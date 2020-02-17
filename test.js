c7210f092538f79698c195131b692bef7e361f43
Jonathan Kemp <kempdogg@gmail.com>
2020-02-17 10:14:44 -0500

    Update tests.

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const pluck = require('./');
 
 describe('pluck', () => {
-	it('should look through the list and return the first value that matches all of the key-value pairs provided', () => {
+	it('should look through the list and return an array of values that matches the key provided', () => {
 		const stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
 		const test = obj => pluck(stooges, obj);
 		assert.deepEqual(test('name'), ['moe', 'larry', 'curly']);
