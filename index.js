ea6e98919486e9aef110f52fd61fc1b1fd1a342e
Jonathan Kemp <kempdogg@gmail.com>
2020-02-17 14:45:35 -0500

    Remove comments.

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const deepGet = (obj, path) => {
 	return length ? obj : void 0;
 };
 
-// Creates a function that, when passed an object, will traverse that object’s
-// properties down the given `path`, specified as an array of keys or indexes.
 const property = path => {
 	if (!Array.isArray(path)) {
 		return shallowProperty(path);
@@ -18,5 +16,4 @@ const property = path => {
 	return obj => deepGet(obj, path);
 };
 
-// Convenience version of a common use case of `map`: fetching a property.
 module.exports = (obj, key) => obj.map(property(key));
