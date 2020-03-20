4049eedcbe6bc25475a0926cf5c1669a94689a6a
Jonathan Kemp <kempdogg@gmail.com>
2020-03-19 23:50:39 -0400

    Add tests.

--- test.js
+++ test.js
@@ -8,4 +8,14 @@ describe('pluck', () => {
 		assert.deepEqual(test('name'), ['moe', 'larry', 'curly']);
 		assert.deepEqual(test('age'), [40, 50, 60]);
 	});
+
+	it('should return missing properties as undefined', () => {
+		const people = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
+
+		assert.deepEqual(pluck(people, 'address'), [void 0, void 0, void 0]);
+	});
+
+	it('should handle objects as keys', () => {
+		assert.deepEqual(pluck([{'[object Object]': 1}], {}), [1]);
+	});
 });
