491fec407bc7e3d712aafccef4491136aa62172f
Jonathan Kemp <kempdogg@gmail.com>
2020-03-29 09:27:14 -0400

    Update eslint rules.

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ describe('pluck', () => {
 	it('should look through the list and return an array of values that matches the key provided', () => {
 		const stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
 		const test = obj => pluck(stooges, obj);
+
 		assert.deepEqual(test('name'), ['moe', 'larry', 'curly']);
 		assert.deepEqual(test('age'), [40, 50, 60]);
 	});
