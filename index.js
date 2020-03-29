f13486ffd80a1d29ccafb80dcdd22725bb7080d4
Jonathan Kemp <kempdogg@gmail.com>
2020-03-29 09:27:51 -0400

    Add package-utils.

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-const shallowProperty = key => obj => obj == null ? void 0 : obj[key];
-
-const deepGet = (obj, path) => {
-	const length = path.length;
-	for (let i = 0; i < length; i++) {
-		if (obj == null) return void 0;
-		obj = obj[path[i]];
-	}
-	return length ? obj : void 0;
-};
-
-const property = path => {
-	if (!Array.isArray(path)) {
-		return shallowProperty(path);
-	}
-	return obj => deepGet(obj, path);
-};
+const { property } = require('@jonkemp/package-utils');
 
 module.exports = (obj, key) => obj.map(property(key));
